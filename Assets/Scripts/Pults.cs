f564d5e1be0d85fc37895084c4b2f4db6a19c013
KkManagalian <Dumbassyahoo2@gmail.com>
2026-02-04 11:08:14 +0200

    Sis tas un viss kaut kas

--- Assets/Scripts/Pults.cs
+++ Assets/Scripts/Pults.cs
@@ -4,26 +4,31 @@ using UnityEngine.UI;
 public class Pults : MonoBehaviour
 {
     public GameObject Background;
-    public GameObject ChannelL;
+    public GameObject Bean;
     public GameObject ChannelR;
     public GameObject SoundUp;
     public GameObject SoundDown;
     public Sprite[] characterSprite;
-    public int Kanals;
 
     public void TurnOn()
     {
-        if(Background != true)
+        if(Background.activeSelf==false)
             Background.SetActive(true);
         else
         {
             Background.SetActive(false);
+            Bean.SetActive(false);
         }
-        
-
     }
-    public void ChangeBackground()
+    public void ChangeBackground(int Kanals)
     {
         Background.GetComponent<Image>().sprite = characterSprite[Kanals];
+
+        if (Bean.activeSelf == false)
+            Bean.SetActive(true);
+        else
+        {
+            Bean.SetActive(false);
+        }
     }
 }
