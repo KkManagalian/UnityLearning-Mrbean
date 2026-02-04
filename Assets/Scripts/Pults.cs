50fc494a5dd5a16f81e31da0a4948d0294a29c83
KkManagalian <Dumbassyahoo2@gmail.com>
2026-02-04 11:34:10 +0200

    Skanas un otra aina un kaut kas

--- Assets/Scripts/Pults.cs
+++ Assets/Scripts/Pults.cs
@@ -5,9 +5,11 @@ public class Pults : MonoBehaviour
 {
     public GameObject Background;
     public GameObject Bean;
-    public GameObject ChannelR;
-    public GameObject SoundUp;
-    public GameObject SoundDown;
+    public GameObject Pipe;
+    public GameObject Bean2D;
+    public GameObject Teddy;
+    public AudioClip[] Sound;
+    public AudioSource sfxSource;
     public Sprite[] characterSprite;
 
     public void TurnOn()
@@ -18,17 +20,30 @@ public class Pults : MonoBehaviour
         {
             Background.SetActive(false);
             Bean.SetActive(false);
+            Bean2D.SetActive(false);
+            Teddy.SetActive(false);
         }
     }
     public void ChangeBackground(int Kanals)
     {
+        sfxSource.Pause();
         Background.GetComponent<Image>().sprite = characterSprite[Kanals];
 
         if (Bean.activeSelf == false)
+        {
             Bean.SetActive(true);
+            Bean2D.SetActive(false);
+            Teddy.SetActive(false);
+            sfxSource.PlayOneShot(Sound[Kanals]);
+
+        }
         else
         {
             Bean.SetActive(false);
+            Bean2D.SetActive(true);
+            Teddy.SetActive(true);
+            sfxSource.PlayOneShot(Sound[Kanals]);
         }
     }
+
 }
