4646e84de7ac71cd0c5e2d15580aaa5f00b1428a
KkManagalian <Dumbassyahoo2@gmail.com>
2026-02-11 10:45:53 +0200

    Jiggle jiggle skin

---
+++ Assets/Scripts/DESTROY.cs
@@ -0,0 +1,25 @@
+using TMPro;
+using UnityEngine;
+
+public class DESTROY : MonoBehaviour
+{
+    OmegaSounds sfx;
+    public TMP_Text counterText;
+    private int count=0;
+
+    void Start()
+    {
+        sfx = FindFirstObjectByType<OmegaSounds>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Doonut"))
+        {
+            Destroy(collision.gameObject);
+            count++;
+            sfx.PlaySFX(0);
+            counterText.text="Donuts destroyed:\n"+count;
+        }
+    }
+}
